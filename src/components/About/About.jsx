c07e4c036368910d7a4777a75ef0920eabe59fcf
Rahida Rashidli <reshidlirahide@gmail.com>
2025-04-12 15:18:15 +0400

    new commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./about.module.css";
+import styles from "./About.module.css";
 import { FaLeaf, FaHandHoldingHeart } from "react-icons/fa";
 import { useTranslation } from "react-i18next";
 
